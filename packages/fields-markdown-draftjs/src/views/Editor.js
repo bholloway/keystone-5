402a08ec6f329905ed44c43e926c4f9652ea7265
Alex Lytvynenko <lytvynenko@outlook.com>
2019-06-05 14:29:34 +1000

    Added toggle to switch markdown field between raw and preview modes

--- packages/fields-markdown-draftjs/src/views/Editor.js
+++ packages/fields-markdown-draftjs/src/views/Editor.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { jsx, css } from '@emotion/core';
-import { useState, useEffect, useMemo, useRef } from 'react';
+import { useState, useEffect, useMemo, useRef, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import compose from 'compose-function';
 import PluginEditor from 'draft-js-plugins-editor';
@@ -9,14 +9,20 @@ import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
 import { mdToDraftjs, draftjsToMd } from 'draftjs-md-converter';
 import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 
-const encodeEditorState = compose(EditorState.createWithContent, convertFromRaw, mdToDraftjs);
-const decodeEditorState = compose(draftjsToMd, convertToRaw, v => v.getCurrentContent());
+const encodeEditorState = compose(
+  EditorState.createWithContent,
+  convertFromRaw,
+  mdToDraftjs
+);
+const decodeEditorState = compose(
+  draftjsToMd,
+  convertToRaw,
+  v => v.getCurrentContent()
+);
 
 const Editor = ({ value, id, name, isDisabled, autoFocus, onChange }) => {
-  const plugins = useMemo(() => [
-    createMarkdownShortcutsPlugin(),
-  ], []);
-  const [ editorState, onEditorState ] = useState(encodeEditorState(value));
+  const plugins = useMemo(() => [createMarkdownShortcutsPlugin()], []);
+  const [editorState, onEditorState] = useState(encodeEditorState(value));
   const ref = useRef();
   useEffect(() => {
     onEditorState(encodeEditorState(value));
@@ -24,26 +30,19 @@ const Editor = ({ value, id, name, isDisabled, autoFocus, onChange }) => {
       ref.current.focus();
     }
   }, []);
-  useEffect(() => onChange(decodeEditorState(editorState)), [ value ]);
-  
+  useEffect(() => onChange(decodeEditorState(editorState)), [value]);
+
   return (
-    <div css={css`
-      width: 100%;
-      padding: 8px;
-      border: 1px solid #c1c7d0;
-      border-radius: 5px;
-    `}>
-      <PluginEditor
-        ref={ref}
-        id={id}
-        name={name}
-        editorState={editorState}
-        onChange={isDisabled ? undefined : onEditorState}
-        plugins={plugins}
-      />
-    </div>
+    <PluginEditor
+      ref={ref}
+      id={id}
+      name={name}
+      editorState={editorState}
+      onChange={isDisabled ? undefined : onEditorState}
+      plugins={plugins}
+    />
   );
-}
+};
 
 Editor.propTypes = {
   autoFocus: PropTypes.bool,
--- packages/fields-markdown-draftjs/src/views/Field.js
+++ packages/fields-markdown-draftjs/src/views/Field.js
@@ -1,21 +1,55 @@
 /** @jsx jsx */
 
-import { jsx } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
 import { Component } from 'react';
-
 import { FieldContainer, FieldLabel } from '@arch-ui/fields';
+import { MarkdownIcon, CodeIcon } from '@arch-ui/icons';
+import Tooltip from '@arch-ui/tooltip';
+import { IconButton } from '@arch-ui/button';
 
 import Editor from './Editor';
 
+const PreviewToggleIcon = ({ previewEnabled, onClick }) => (
+  <Tooltip
+    content={`${previewEnabled ? 'Disable' : 'Enable'} preview mode`}
+    hideOnMouseDown
+    hideOnKeyDown
+  >
+    {ref => (
+      <IconButton
+        ref={ref}
+        iconSize={16}
+        variant="ghost"
+        css={css`
+          position: absolute;
+          right: 8px;
+          top: 8px;
+        `}
+        icon={previewEnabled ? MarkdownIcon : CodeIcon}
+        onClick={onClick}
+      />
+    )}
+  </Tooltip>
+);
+
 export default class MarkdownField extends Component {
+  state = {
+    showPreview: false,
+  };
   onChange = value => {
     if (typeof value === 'string' && value !== this.props.value) {
       this.props.onChange(value);
     }
   };
+  onPreviewToggle = () => {
+    this.setState({ showPreview: !this.state.showPreview });
+  };
+
   render() {
     const { autoFocus, field, error, value: serverValue } = this.props;
+    const { showPreview } = this.state;
     const value = serverValue || '';
+    const rows = value.split('\n').length;
     const htmlID = `ks-input-${field.path}`;
     const canRead = !(error instanceof Error && error.name === 'AccessDeniedError');
 
@@ -33,8 +67,37 @@ export default class MarkdownField extends Component {
         >
           {field.label}
         </FieldLabel>
-        <div css={{ display: 'flex', flex: 1 }}>
-          <Editor value={value} onChange={this.onChange} id={htmlID} autoFocus={autoFocus} />
+        <div
+          css={css`
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #c1c7d0;
+            border-radius: 5px;
+            position: relative;
+            min-height: 50px;
+            max-width: 640px;
+            box-sizing: border-box;
+          `}
+        >
+          {showPreview ? (
+            <Editor value={value} onChange={this.onChange} id={htmlID} autoFocus={autoFocus} />
+          ) : (
+            <textarea
+              rows={rows}
+              css={css`
+                width: 100%;
+                border: none;
+                min-height: 50px;
+                font-size: 1rem;
+                outline: none;
+                box-sizing: border-box;
+                padding-right: 48px;
+              `}
+              value={value}
+              onChange={e => this.props.onChange(e.target.value)}
+            />
+          )}
+          <PreviewToggleIcon onClick={this.onPreviewToggle} previewEnabled={showPreview} />
         </div>
       </FieldContainer>
     );
